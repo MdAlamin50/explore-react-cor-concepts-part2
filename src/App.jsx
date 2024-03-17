79b5f98e6072d09e421adb4d4337063b451db56b
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-03-18 02:11:20 +0600

    added the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleClick() {
+    alert("button clicked");
+  }
+  const handleClick2 = () => {
+    alert("button clicked2");
+  };
+
+  const addFore = (num) => {
+    alert(num + 5);
+  };
 
   return (
     <>
+      <h3>React Core Concepts 2</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click22</button>
+      <button
+        onClick={() => {
+          alert("click3");
+        }}
+      >
+        Click3
+      </button>
       
-      <h1>Vite + React</h1>
-     
+      <button onClick={() =>addFore(3)}>Click Four</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
